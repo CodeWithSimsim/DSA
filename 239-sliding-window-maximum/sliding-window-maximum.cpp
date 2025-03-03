73d5dd499d3d13c34b58a94ef13ce9d28c1c21c0
..simsim.. <97021123+CodeWithSimsim@users.noreply.github.com>
2025-03-04 00:11:32 +0530

    Time: 407 ms (5.78%) | Memory: 216.5 MB (5.52%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        multiset<int>ms;
+        for(int i =0; i<k-1; i++)
+        {
+            ms.insert(nums[i]);
+        }
+        int start = 0;
+        vector<int>ans;
+
+        for(int i = k-1; i<n; i++)
+        {
+            ms.insert(nums[i]);
+            int curMax = *ms.rbegin();
+            ans.push_back(curMax);
+            auto it = ms.find(nums[start]);
+            if (it != ms.end()) {
+                ms.erase(it);
+            }
+            start++;
+        }
+        return ans;
+    }
+};
