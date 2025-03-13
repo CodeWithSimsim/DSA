761a236ef0646dab0ed6096d1364b86a716f09e1
..simsim.. <97021123+CodeWithSimsim@users.noreply.github.com>
2025-03-13 10:29:21 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (43.64%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,13 +1,48 @@
 class Solution {
 public:
+    int cntP(int left, int right, vector<int>& nums)
+    {
+        int n = nums.size();
+        int ind = n;
+        while(left <= right)
+        {
+            int mid = left + (right - left)/2;
+            if(nums[mid] > 0)
+            {
+                ind = mid;
+                right = mid-1;
+            }
+            else
+            {
+                left = mid+1;
+            }
+        }
+        return ind;
+    }
+
+    int cntN(int left, int right, vector<int>& nums)
+    {
+        int ind = -1;
+        while(left <= right)
+        {
+            int mid = left + (right - left)/2;
+            if(nums[mid] < 0)
+            {
+                ind = mid;
+                left = mid+1;
+            }
+            else right = mid-1;
+        }
+        return ind;
+    }
     int maximumCount(vector<int>& nums) {
         int n = nums.size();
-        int cntN =0, cntP =0;
-        for(int i =0; i<n; i++)
-        {
-            if(nums[i] < 0) cntN++;
-            if(nums[i] > 0) cntP++;
-        }
-        return max(cntN, cntP);
+        int indP = cntP(0, n-1, nums);
+        int indN = cntN(0, n-1, nums);
+        if(indP == n && indN == -1) return 0;
+        if(indP == n) return indN+1;
+        if(indN == n) return n- indP;
+        int maxCnt = max(n-indP, indN+1);
+        return maxCnt;
     }
 };
