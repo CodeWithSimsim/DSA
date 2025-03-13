5bb19267f6ce7325e2350740fe39472093535123
..simsim.. <97021123+CodeWithSimsim@users.noreply.github.com>
2025-03-13 10:40:15 +0530

    Time: 0 ms (100.00%) | Memory: 21.2 MB (95.51%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,48 +1,25 @@
 class Solution {
 public:
-    int cntP(int left, int right, vector<int>& nums)
-    {
-        int n = nums.size();
-        int ind = n;
-        while(left <= right)
-        {
-            int mid = left + (right - left)/2;
-            if(nums[mid] > 0)
-            {
-                ind = mid;
-                right = mid-1;
-            }
-            else
-            {
-                left = mid+1;
-            }
-        }
-        return ind;
+    int maximumCount(vector<int>& nums) {
+        int neg_count = binarySearch(nums, 0);
+        int pos_count = nums.size() - binarySearch(nums, 1);
+        return max(neg_count, pos_count);
     }
 
-    int cntN(int left, int right, vector<int>& nums)
-    {
-        int ind = -1;
-        while(left <= right)
-        {
-            int mid = left + (right - left)/2;
-            if(nums[mid] < 0)
-            {
-                ind = mid;
-                left = mid+1;
+private:
+    int binarySearch(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1, result = nums.size();
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                result = mid;
+                right = mid - 1;
             }
-            else right = mid-1;
         }
-        return ind;
-    }
-    int maximumCount(vector<int>& nums) {
-        int n = nums.size();
-        int indP = cntP(0, n-1, nums);
-        int indN = cntN(0, n-1, nums);
-        if(indP == n && indN == -1) return 0;
-        if(indP == n) return indN+1;
-        if(indN == n) return n- indP;
-        int maxCnt = max(n-indP, indN+1);
-        return maxCnt;
+        
+        return result;
     }
 };
