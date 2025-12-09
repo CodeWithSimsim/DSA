74f4ad624f3fca457195ff42438e9b07c13ba0d7
..simsim.. <97021123+CodeWithSimsim@users.noreply.github.com>
2025-12-09 20:21:16 +0530

    Time: 37 ms (30.39%) | Memory: 45.4 MB (5.99%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -19,6 +19,29 @@ public:
         int n1= text1.size();
         int n2 = text2.size();
         vector<vector<int>>dp(n1, vector<int>(n2, -1));
-        return helper(n1-1, n2-1, text1, text2, dp);
+        //return helper(n1-1, n2-1, text1, text2, dp);
+
+        vector<vector<int>>dp2(n1, vector<int>(n2, 0));
+        for(int i =0; i<n1; i++)
+        {
+            for(int j =0; j<n2; j++)
+            {
+                if(text1[i] == text2[j])
+                {
+                    int pre = 0;
+                    if(i-1 >=0 && j-1 >=0) pre = dp[i-1][j-1];
+                    dp[i][j] = 1 + pre;
+
+                }
+                else 
+                {
+                    int pr = 0, pc =0;
+                    if(i-1 >=0) pr = dp[i-1][j];
+                    if(j-1 >=0) pc = dp[i][j-1];
+                    dp[i][j] = max(pr, pc);
+                }
+            }
+        }
+        return dp[n1-1][n2-1];
     }
 };
