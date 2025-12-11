7ca16db8d3df6c7e5c5f396deb6ab2dc20e721ad
..simsim.. <97021123+CodeWithSimsim@users.noreply.github.com>
2025-12-12 00:27:31 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (55.71%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -3,34 +3,25 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size();
         int m = image[0].size();
-
+        if(image[sr][sc] == color) return image;
         queue<pair<int, int>>que;
-        vector<vector<int>>vis(n, vector<int>(m, 0));
-        que.push({sr, sc});
-        vis[sr][sc] =1;
-        int stColor = image[sr][sc];
+        int start = image[sr][sc];
         image[sr][sc] = color;
-
+        que.push({sr, sc});
         int dr[4] = {-1, 0, 1, 0};
         int dc[4] = {0, 1, 0, -1};
-
         while(!que.empty())
         {
-            auto top = que.front();
+            auto [r, c] = que.front();
             que.pop();
-            int crow = top.first;
-            int ccol = top.second;
-
             for(int i =0; i<4; i++)
             {
-                int nrow = crow + dr[i];
-                int ncol = ccol + dc[i];
-
-                if(nrow >=0 && nrow < n && ncol >=0 && ncol <m && image[nrow][ncol] == stColor && vis[nrow][ncol] == 0)
+                int nr = r +dr[i];
+                int nc = c + dc[i];
+                if(nr >=0 && nr <n && nc >=0 && nc <m && image[nr][nc] == start)
                 {
-                    image[nrow][ncol] = color;
-                    vis[nrow][ncol] = 1;
-                    que.push({nrow, ncol});
+                    image[nr][nc] = color;
+                    que.push({nr, nc});
                 }
             }
         }
