05d99dd5172b8d3a1da9a21ece464c418ebf9531
..simsim.. <97021123+CodeWithSimsim@users.noreply.github.com>
2026-02-23 13:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (94.71%) - LeetSync

--- 899-binary-gap/binary-gap.cpp
+++ 899-binary-gap/binary-gap.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int binaryGap(int n) {
-        int first = -1; 
-        int second = -1;
-        int pos = 0;
+        int pre = -1; 
+        
+        int cur = 0;
         int ans = 0;
         while(n)
         {
             if(n & 1)
             {
-                second = first;
-                first = pos;
-                if(second != -1)
-                ans = max(ans, (first - second));
+                if(pre != -1)
+                ans = max(ans, (cur - pre));
+                pre = cur;
             }
             n = n>>1;
-            pos++;
+            cur++;
         }
         return ans;
     }
